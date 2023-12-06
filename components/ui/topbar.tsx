f381e6b517856d9706d60009e4c5db408ec6d645
alirafli <mrafli1212@gmail.com>
2023-12-07 00:56:55 +0700

    feat: add responsive sidebar behavior

--- components/ui/topbar.tsx
+++ components/ui/topbar.tsx
@@ -1,16 +1,40 @@
+"use client";
+
 import React from "react";
 import { ModeToggle } from "../toggle-theme";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { User } from "@/types/user";
+import { useAppDispatch, useAppSelector } from "@/hooks/useRedux";
+import { toggleCollapsedSideNav } from "@/lib/redux/features/settingsSlice";
+import { IoClose } from "react-icons/io5";
 
 interface TopBarProps {
   user?: User;
 }
 
 export default function Topbar({ user }: TopBarProps) {
+  const dispatch = useAppDispatch();
+
+  const { navCollapsed } = useAppSelector((state) => state.settingsReducer);
+  const handleToggleSidebar = () => {
+    dispatch(toggleCollapsedSideNav(!navCollapsed));
+  };
+
   return (
     <div className="flex justify-between bg-zinc-200 dark:bg-zinc-900 shadow-lg dark:shadow-lg-dark px-3 md:px-14 py-2 items-center mb-6">
-      <RxHamburgerMenu size={20} className="cursor-pointer" />
+      {navCollapsed ? (
+        <IoClose
+          size={20}
+          className="cursor-pointer"
+          onClick={handleToggleSidebar}
+        />
+      ) : (
+        <RxHamburgerMenu
+          size={20}
+          className="cursor-pointer"
+          onClick={handleToggleSidebar}
+        />
+      )}
 
       <div className="flex items-center gap-8">
         <div>
