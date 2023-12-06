187d154429d4c712e9f1b256b68a008ce0f6cb7d
alirafli <mrafli1212@gmail.com>
2023-12-06 11:25:41 +0700

    feat(temp): add signout button

--- app/(root)/(dashboard)/page.tsx
+++ app/(root)/(dashboard)/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { redirect } from "next/navigation";
 import { readUserSession } from "@/lib/actions";
+import { createSupabaseServerClient } from "@/lib/supabase/server";
+import { Button } from "@/components/ui/button";
 
 async function Dashboard() {
   const { data } = await readUserSession();
@@ -8,9 +10,21 @@ async function Dashboard() {
     return redirect("/sign-in");
   }
 
+  const logout = async () => {
+    "use server";
+    const supabse = await createSupabaseServerClient();
+    await supabse.auth.signOut();
+    redirect("/sign-in");
+  };
+
   return (
     <div>
       <h1>Dashboard</h1>
+      <div>
+        <form action={logout}>
+          <Button>SignOut</Button>
+        </form>
+      </div>
     </div>
   );
 }
