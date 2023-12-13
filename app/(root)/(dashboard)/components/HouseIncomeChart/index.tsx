35e006f9464aef6434f68c187fbaeaec2c9a47cc
alirafli <mrafli1212@gmail.com>
2023-12-14 00:36:29 +0700

    feat: add bar and pie chart

--- app/(root)/(dashboard)/components/HouseIncomeChart/index.tsx
+++ app/(root)/(dashboard)/components/HouseIncomeChart/index.tsx
@@ -1,9 +1,26 @@
 "use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 function HouseIncomePie() {
+  const data = {
+    labels: ["Pemasukan", "Pengeluaran"],
+    datasets: [
+      {
+        label: "Rp",
+        data: [12, 19],
+        backgroundColor: ["rgba(255, 99, 132, 0.3)", "rgba(75, 192, 192, 0.3)"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(75, 192, 192, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <Card className="ml-0 md:ml-6 w-full md:w-[30%] mt-6 md:mt-0">
       <CardHeader>
@@ -11,7 +28,9 @@ function HouseIncomePie() {
       </CardHeader>
 
       <CardContent>
-        <div className="h-80 w-full bg-red-500"></div>
+        <div className="h-80 w-full">
+          <Pie data={data} />
+        </div>
       </CardContent>
     </Card>
   );
--- app/(root)/(dashboard)/components/IncomeChart/index.tsx
+++ app/(root)/(dashboard)/components/IncomeChart/index.tsx
@@ -1,7 +1,72 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+"use client";
+
 import React from "react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 function IncomeChart() {
+  const options = {
+    maintainAspectRatio: false,
+    responsive: true,
+
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Chart.js Bar Chart",
+      },
+    },
+  };
+
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Pemasukan",
+        data: [1, 2, 3, 4, 5, 6, 7],
+        backgroundColor: "rgba(255, 99, 132, 0.3)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        borderWidth: 1,
+      },
+      {
+        label: "Pengeluaran",
+        data: [7, 6, 5, 4, 3, 2, 1],
+        backgroundColor: "rgba(75, 192, 192, 0.3)",
+        borderColor: "rgba(75, 192, 192, 1)",
+        borderWidth: 1,
+      },
+    ],
+  };
   return (
     <Card className="mt-6">
       <CardHeader>
@@ -9,7 +74,9 @@ function IncomeChart() {
       </CardHeader>
 
       <CardContent>
-        <div className="h-80 w-full bg-red-500"></div>
+        <div className="h-80 w-full">
+          <Bar options={options} data={data} />
+        </div>
       </CardContent>
     </Card>
   );
