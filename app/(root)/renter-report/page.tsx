20f0c5788347835b9961f2e091c2125f4c5eb074
alirafli <mrafli1212@gmail.com>
2023-12-22 22:48:39 +0700

    remove ununsed code

--- app/(root)/renter-report/page.tsx
+++ app/(root)/renter-report/page.tsx
@@ -1,18 +1,12 @@
 import { Metadata } from "next";
 import React from "react";
-import TestRender from "./components";
 
 export const metadata: Metadata = {
   title: "Laporan Penyewa",
 };
 
 function page() {
-  return (
-    <div className="pl-6">
-      renter report page
-      <TestRender />
-    </div>
-  );
+  return <div className="pl-6">renter report page</div>;
 }
 
 export default page;
